0d8f9628f6d701b20bf496b982641982055daa76
HoangAn <hoangan23082004@gmail.com>
2025-04-22 22:01:46 +0700

    Bai tap buoi 1

---
+++ BT_Buoi1/InPhanTuBeHon5.dart
@@ -0,0 +1,9 @@
+void main(){
+    List <int> array = [1,1,2,3,5,8,13,21,34,55,89];
+    print("Nhung so be hon 5 la: ");
+    for(int i = 0; i <11 ; i++){
+        if(array[i] < 5){
+            print('$i, ');
+        }
+    }
+}
