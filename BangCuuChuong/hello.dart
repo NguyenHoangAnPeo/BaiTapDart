48d09212cf7d1f081db45ef0f83446f52920c956
HoangAn <hoangan23082004@gmail.com>
2025-04-22 11:22:01 +0700

    Bai tap bang cuu chuong

---
+++ BangCuuChuong/hello.dart
@@ -0,0 +1,11 @@
+void main(){
+    int tich;
+   for(int i = 1; i<=10; i++){
+    print('---Bang cuu chuong $i ---');
+    for(int j = 1; j<=10; j++){
+        tich = i*j;
+        print('$i * $j = $tich');
+    }
+   }
+   print(' ');
+}
